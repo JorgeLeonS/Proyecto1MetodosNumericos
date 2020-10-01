438cd7a11fd4d037388ea1f4b6ef42cadfbfe1f7
Jorge León Salas <36204728+JorgeLeonS@users.noreply.github.com>
2020-10-01 13:06:32 -0500

    CongruencialLineal
    Se creó el método de Congruencial Lineal

---
+++ NumerosAleatorios/Assets/Scripts/CongruencialLineal.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CongruencialLineal : MonoBehaviour
+{
+    public InputField x0Input;
+    public InputField aInput;
+    public InputField cInput;
+    public InputField mInput;
+    public InputField nInput;
+
+    public GameObject Row;
+    public GameObject Semilla;
+    public GameObject Generador;
+    public GameObject Numero_aleatorio;
+    public GameObject Ri;
+    public GameObject Content;
+
+    public void generarNumeros(){
+        int x0 = int.Parse(x0Input.text);
+        int a = int.Parse(aInput.text);
+        int c = int.Parse(cInput.text);
+        int m = int.Parse(mInput.text);
+        int n = int.Parse(nInput.text);
+
+        string x0String = x0Input.text;
+
+        // print(x0 + a + c + m);
+
+        //Se repite las veces que quiera el usuario
+        for(int i = 0;i<n;i++){
+            int generador = ((x0 * a) + c);
+
+            print("Generador "+generador);
+
+            int aleatorio = generador % m;
+
+            float aleatorio_float  = (float)aleatorio;
+            float  m_float = (float)m;
+            float ri = aleatorio_float / m_float;
+
+            createNewRow(x0.ToString(), generador.ToString(), aleatorio.ToString(), ri);
+
+            print("aleatorio "+aleatorio);
+            print("ri"+ri.ToString("0.000"));
+
+            x0 = aleatorio;
+        }
+
+
+
+    }
+
+    public void createNewRow(string semilla, string generador, string nAleatorio, float ri){
+        //Instanciar nueva fila
+        GameObject new_row = Instantiate(Row,new Vector3(0,0,0) , Quaternion.identity) as GameObject;
+        //Unirla a la tabla
+        new_row.transform.SetParent (Content.transform, false);
+        
+        //Unir los objetos de texto con el codigo
+        Semilla = new_row.transform.Find("Semilla").gameObject;
+        Generador = new_row.transform.Find("Generador").gameObject;
+        Numero_aleatorio = new_row.transform.Find("Numero aleatorio").gameObject;
+        Ri = new_row.transform.Find("Ri").gameObject;
+
+        //Poner los valores correspondientes
+        Semilla.GetComponent<Text>().text =semilla;
+        Generador.GetComponent<Text>().text = generador;
+        Numero_aleatorio.GetComponent<Text>().text = nAleatorio;
+        Ri.GetComponent<Text>().text= ri.ToString("0.000");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
