0e1d5482f60bda5975df3a8e8e87c24fed0ddb17
Jorge León Salas <36204728+JorgeLeonS@users.noreply.github.com>
2020-10-02 18:18:53 -0500

    MetodosRestantes
    Se agregaron los 3 metodos restantes, quedando pendiente el desarrollo de pruebas chi cuadrada y kolmogorov smirnov. Además de arreglar la inerfaz, documentos y verificar funcionalidad con el profesor.

---
+++ NumerosAleatorios/Assets/Scripts/CongruencialLinealCombinado.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CongruencialLinealCombinado : MonoBehaviour
+{
+
+    public InputField xInput;
+    public InputField yInput;
+
+    public InputField a1Input;
+    public InputField a2Input;
+    // public InputField cInput;
+    public InputField m1Input;
+    public InputField m2Input;
+    public InputField m3Input;
+
+    public GameObject Row;
+    public GameObject Semilla;
+    public GameObject Generador;
+    public GameObject Numero_aleatorio;
+    public GameObject Ri;
+    public GameObject Content;
+
+    public void generarNumeros(){
+        int x0 = int.Parse(xInput.text);
+        int y0 = int.Parse(yInput.text);
+        int a1 = int.Parse(a1Input.text);
+        int a2 = int.Parse(a2Input.text);
+        int m1 = int.Parse(m1Input.text);
+        int m2 = int.Parse(m2Input.text);
+        int m3 = int.Parse(m3Input.text);
+
+        int xn = x0;
+        int yn = y0;
+
+        int contador = 0;
+        int w = 0;
+
+        do{
+            if(xn-yn < 0){
+                w = m3 - Mathf.Abs(xn-yn);
+            }else{
+                w = (xn - yn) % m3;
+            }
+
+            createNewRow(contador, xn, yn, w);
+            contador++;
+
+            xn = (a1*xn) % m1;
+            print("xn"+xn);
+            yn = (a2*yn) % m2;
+            print("yn"+yn);
+            
+
+        }while(xn != x0 || yn != y0);
+
+    }
+
+    public void createNewRow(int c, int x, int y, int w){
+        //Instanciar nueva fila
+        GameObject new_row = Instantiate(Row,new Vector3(0,0,0) , Quaternion.identity) as GameObject;
+        //Unirla a la tabla
+        new_row.transform.SetParent (Content.transform, false);
+        
+        //Unir los objetos de texto con el codigo
+        Semilla = new_row.transform.Find("Semilla").gameObject;
+        Generador = new_row.transform.Find("Generador").gameObject;
+        Numero_aleatorio = new_row.transform.Find("Numero aleatorio").gameObject;
+        Ri = new_row.transform.Find("Ri").gameObject;
+
+        //Poner los valores correspondientes
+        Semilla.GetComponent<Text>().text =c.ToString();
+        Generador.GetComponent<Text>().text = x.ToString();
+        Numero_aleatorio.GetComponent<Text>().text = y.ToString();
+        Ri.GetComponent<Text>().text= w.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NumerosAleatorios/Assets/Scripts/CongruencialLinealMixto.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CongruencialLinealMixto : MonoBehaviour
+{
+    public InputField x0Input;
+    public InputField aInput;
+    public InputField cInput;
+    public InputField mInput;
+    public InputField nInput;
+
+    public GameObject Row;
+    private GameObject Semilla;
+    private GameObject Generador;
+    private GameObject Numero_aleatorio;
+    private GameObject Ri;
+    public GameObject Content;
+    public GameObject HullDobelltext;
+
+    public void generarNumeros(){
+        int x0 = int.Parse(x0Input.text);
+        int a = int.Parse(aInput.text);
+        int c = int.Parse(cInput.text);
+        int m = int.Parse(mInput.text);
+        int n = m;
+
+        if(!HullDobell(a,c,m)){
+            HullDobelltext.GetComponent<Text>().text = "No jalo";
+        }else{
+            HullDobelltext.GetComponent<Text>().text = "Ta bien";
+            string x0String = x0Input.text;
+
+            // print(x0 + a + c + m);
+
+            //Se repite las veces que quiera el usuario
+            for(int i = 0;i<n;i++){
+                int generador = ((x0 * a) + c);
+
+                // print("Generador "+generador);
+
+                int aleatorio = generador % m;
+
+                float aleatorio_float  = (float)aleatorio;
+                float  m_float = (float)m;
+                float ri = aleatorio_float / m_float;
+
+                createNewRow(x0.ToString(), generador.ToString(), aleatorio.ToString(), ri);
+
+                // print("aleatorio "+aleatorio);
+                // print("ri"+ri.ToString("0.000"));
+
+                x0 = aleatorio;
+            }
+        }
+
+    }
+
+    public bool HullDobell(int a, int c, int m){
+        // Sea c y m primos relativo
+        if(Mcd(c,m) != 1) return false;
+
+        List<int> primeNumbers = new List<int>();
+
+        primeNumbers = PrimeFactors(m);
+
+        for (int i = 0; i < primeNumbers.Count; i++) {
+            if ((a - 1) % primeNumbers[i] != 0) return false;
+        }
+
+        if (m % 4 == 0) {
+            if ((a - 1) % 4 != 0) return false;
+        }
+
+        return true;
+
+    }
+
+    public int Mcd(int a, int b){
+        
+    if (a == 0 || b == 0) return 0;
+
+    while (a != b) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+    }
+        return a;
+    }
+
+    public List<int> PrimeFactors(int n) {
+        List<int> primeNumbers = new List<int>();
+
+        while (n % 2 == 0) {
+            primeNumbers.Add(2);
+            n = n / 2;
+        }
+
+        for (int i = 3; i <= Mathf.Sqrt(n); i = i + 2) {
+            while (n % i == 0) {
+                primeNumbers.Add(i);
+                n = n / i;
+            }
+        }
+
+        if (n > 2) primeNumbers.Add(n);
+
+        return primeNumbers;
+        }
+
+
+    public void createNewRow(string semilla, string generador, string nAleatorio, float ri){
+        //Instanciar nueva fila
+        GameObject new_row = Instantiate(Row,new Vector3(0,0,0) , Quaternion.identity) as GameObject;
+        //Unirla a la tabla
+        new_row.transform.SetParent (Content.transform, false);
+        
+        //Unir los objetos de texto con el codigo
+        Semilla = new_row.transform.Find("Semilla").gameObject;
+        Generador = new_row.transform.Find("Generador").gameObject;
+        Numero_aleatorio = new_row.transform.Find("Numero aleatorio").gameObject;
+        Ri = new_row.transform.Find("Ri").gameObject;
+
+        //Poner los valores correspondientes
+        Semilla.GetComponent<Text>().text =semilla;
+        Generador.GetComponent<Text>().text = generador;
+        Numero_aleatorio.GetComponent<Text>().text = nAleatorio;
+        Ri.GetComponent<Text>().text= ri.ToString("0.000");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
